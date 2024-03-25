a2f603021d00587ba0d6d32e4fc3c45fa9d185f4
Wahyu Jebredz <wabredz1234@gmail.com>
2024-03-25 21:39:37 +0700

    feat(server): Implement user login controller and route for Sahaba Fashion project
    This commit adds a new controller and route to the server project for Sahaba Fashion, focusing on user authentication.

--- server/api/controllers/auth-controller.js
+++ server/api/controllers/auth-controller.js
@@ -1,6 +1,8 @@
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 import { pool } from "../config/db.js";
 
+// Controller untuk daftar akun (register)
 export const registerAccount = async (req, res) => {
   const { username, email, password } = req.body;
   try {
@@ -12,6 +14,40 @@ export const registerAccount = async (req, res) => {
     res.status(201).json({ msg: "Pendaftaran berhasil", data: result.rows[0] });
   } catch (error) {
     // console.log(error);
+    res.status(500).json({ error });
+  }
+};
+
+// Controller untuk login
+export const loginAccount = async (req, res) => {
+  const { usernameoremail, password } = req.body;
+  try {
+    // Mencari data user berdasarkan username atau email
+    const result = await pool.query(
+      "SELECT * FROM users WHERE username = $1 OR email = $2",
+      [usernameoremail, usernameoremail]
+    );
+
+    if (result.rows[0]) {
+      // Verifikasi antara password yang diinputkan di client dengan database
+      const isPasswordValid = await argon2.verify(
+        result.rows[0].password,
+        password
+      );
+
+      if (isPasswordValid) {
+        const token = jwt.sign(result.rows[0], process.env.SECRET_KEY);
+        res.send({
+          token,
+          message: "Login berhasil !!!",
+        });
+      } else {
+        return res.status(401).json({ msg: "Password salah !!!" });
+      }
+    } else {
+      return res.status(404).json({ msg: "User tidak ditemukan !!!" });
+    }
+  } catch (error) {
     res.status(500).json({ error });
   }
 };
--- server/api/routes/auth-route.js
+++ server/api/routes/auth-route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getCurrentUser,
+  loginAccount,
   registerAccount,
 } from "../controllers/auth-controller.js";
 
@@ -8,5 +9,6 @@ const router = express.Router();
 
 router.get("/get-current-user", getCurrentUser); // Router untuk mendapatkan data user yang sedang login
 router.post("/register", registerAccount); // Router untuk daftar akun (register)
+router.post("/login", loginAccount); // Router untuk login akun
 
 export default router;
