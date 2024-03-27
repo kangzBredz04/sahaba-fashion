07f05fc9d1f6a7ab5c1e6501bec74f134a8736f4
Wahyu Jebredz <wabredz1234@gmail.com>
2024-03-27 09:05:12 +0700

    feat(server): Update controller to retrieve cart data based on user ID

--- server/api/controllers/cart-controller.js
+++ server/api/controllers/cart-controller.js
@@ -53,10 +53,19 @@ export const addCart = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
-// Controller untuk mendapatkan semua data keranjang
-export const getAllCart = async (_req, res) => {
+
+// Controller untuk mendapatkan data keranjang berdasarkan id user
+export const getCartByIdUser = async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM carts");
+    const result = await pool.query(
+      `
+    SELECT p.image_1, p.name_product, s.name_size, c.total_product, p.price 
+    FROM carts c 
+    JOIN products p ON c.id_product = p.id
+    JOIN sizes s ON c.id_size = s.id WHERE c.id_user = $1
+    `,
+      [req.params.id]
+    );
     res.json(result.rows);
   } catch (error) {
     res.status(500).json({ msg: error.message });
--- server/api/routes/cart-route.js
+++ server/api/routes/cart-route.js
@@ -3,13 +3,13 @@ import { verifyToken } from "../middleware/auth-middleware.js";
 import {
   addCart,
   deleteCart,
-  getAllCart,
+  getCartByIdUser,
   updateCart,
 } from "../controllers/cart-controller.js";
 
 const router = express.Router();
 
-router.get("/get-all", verifyToken, getAllCart); // Router untuk mendapatkan semua keranjang
+router.get("/get-cart/:id", verifyToken, getCartByIdUser); // Router untuk mendapatkan keranjang by id user
 router.post("/add", verifyToken, addCart); // Router untuk menambahkan data keranjang
 router.put("/update/:id", verifyToken, updateCart); // Router untuk mengubah data keranjang berdasarkan id
 router.delete("/delete/:id", verifyToken, deleteCart); // Router untuk menghapus data keranjang berdasarkan id
