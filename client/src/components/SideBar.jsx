d658d191693f309eb5de5bf321725792bdd14e84
Wahyu Jebredz <wabredz1234@gmail.com>
2024-03-29 17:30:29 +0700

    feat(client): Add SideBar component. This commit introduces the SideBar component to the client-side of the Sahaba Fashion project.

---
+++ client/src/components/SideBar.jsx
@@ -0,0 +1,43 @@
+import { MdOutlineDashboard } from "react-icons/md";
+import { GrLogout } from "react-icons/gr";
+import { FiUsers } from "react-icons/fi";
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
+import { LuBoxes } from "react-icons/lu";
+import { RiShirtLine } from "react-icons/ri";
+
+export default function SideBar() {
+  return (
+    <div className="font-KumbhSans border flex flex-col gap-7 shadow-xl h-screen w-36 py-4 px-4">
+      <div className=" text-center">
+        <h1 className="text-xl font-bold tracking-widest">SAHABA</h1>
+        <h1 className="text-xl font-bold tracking-widest">FASHION</h1>
+      </div>
+      <div className="flex flex-col justify-between h-full">
+        <div className="text-center flex flex-col gap-1 hover:cursor-pointer">
+          <MdOutlineDashboard className="m-auto text-3xl" />
+          <h1 className="text-base font-medium">Dashboard</h1>
+        </div>
+        <div className="text-center flex flex-col gap-1 hover:cursor-pointer">
+          <FiUsers className="m-auto text-3xl" />
+          <h1 className="text-base font-medium">Users</h1>
+        </div>
+        <div className="text-center flex flex-col gap-1 hover:cursor-pointer">
+          <RiShirtLine className="m-auto text-3xl" />
+          <h1 className="text-base font-medium">Products</h1>
+        </div>
+        <div className="text-center flex flex-col gap-1 hover:cursor-pointer">
+          <LiaFileInvoiceDollarSolid className="m-auto text-3xl" />
+          <h1 className="text-base font-medium">Orders</h1>
+        </div>
+        <div className="text-center flex flex-col gap-1 hover:cursor-pointer">
+          <LuBoxes className="m-auto text-3xl" />
+          <h1 className="text-base font-medium">Stocks</h1>
+        </div>
+        <div className="text-center flex flex-col gap-1 hover:cursor-pointer">
+          <GrLogout className="m-auto text-3xl" />
+          <h1 className="text-base font-medium">Logout</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
