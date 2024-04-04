ea36805f1d949059b4cb46f9cd8d3c1c2eef7b68
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-04 23:35:48 +0700

    feat(client): Update CardProduct component for wishlist products

--- client/src/components/CardProduct.jsx
+++ client/src/components/CardProduct.jsx
@@ -1,23 +1,38 @@
 /* eslint-disable react/prop-types */
-import { BsBookmark } from "react-icons/bs";
+import { IoBookmark, IoBookmarkOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 import { api } from "../utils";
-export default function CardProduct({ id, name, image, tipe, price }) {
+export default function CardProduct({ id, name, image, tipe, price, status }) {
   const navigate = useNavigate();
+
+  // const dataProduct = {
+  //   id: id,
+  //   name: name,
+  //   image: image,
+  //   tipe: tipe,
+  //   price: price,
+  // };
 
   return (
     <div className="border border-gray-200 hover:cursor-pointer">
-      <BsBookmark
-        onClick={() => {
-          api
-            .post("/wishlist/add", {
-              id_user: localStorage.getItem("id"),
-              id_product: id,
-            })
-            .then((res) => alert(res));
-        }}
-        className="absolute text-2xl  ml-1 mt-2"
-      />
+      {status ? (
+        <IoBookmark
+          onClick={() => {alert(``)}}
+          className="absolute text-2xl  ml-1 mt-2"
+        />
+      ) : (
+        <IoBookmarkOutline
+          onClick={() => {
+            api
+              .post("/wishlist/add", {
+                id_user: localStorage.getItem("id"),
+                id_product: id,
+              })
+              .then((res) => alert(res));
+          }}
+          className="absolute text-2xl  ml-1 mt-2"
+        />
+      )}
       <div
         className="w-full"
         onClick={() => {
@@ -31,6 +46,7 @@ export default function CardProduct({ id, name, image, tipe, price }) {
         <p className="text-sm">{tipe}</p>
         <p className="text-sm font-semibold">{name}</p>
         <p className="text-sm font-light">Rp{price}</p>
+        {/* <p>{status}</p> */}
       </div>
     </div>
   );
