fee2d44ca0f8415ad69d261cd5060ffa2fce485f
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-11 01:09:07 +0700

    feat(server): Add function to retrieve orders based on user ID

--- server/api/controllers/order-controller.js
+++ server/api/controllers/order-controller.js
@@ -4,10 +4,11 @@ export const getOrderByIdUser = async (req, res) => {
   try {
     const result = await pool.query(
       `
-      SELECT p.image_1, p.name_product, s.name_size, c.total_product, p.price 
-      FROM carts c 
-      JOIN products p ON c.id_product = p.id
-      JOIN sizes s ON c.id_size = s.id WHERE c.id_user = $1
+      SELECT u.username, p.name_product, s.name_size, o.quantity, o.payment_method, o.address, o.status
+      FROM orders o
+      JOIN users u ON o.id_user = u.id
+      JOIN products p ON o.id_product = p.id
+      JOIN sizes s ON o.id_size = s.id WHERE o.id_user = $1
     `,
       [req.params.id]
     );
