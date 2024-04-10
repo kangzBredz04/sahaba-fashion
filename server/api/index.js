19a8edbfbb1dc7ee8225e068b9b39ceee02f704f
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-11 01:08:27 +0700

    feat(server): Add main route for orders

--- server/api/index.js
+++ server/api/index.js
@@ -9,6 +9,7 @@ import SizeRoute from "./routes/size-route.js";
 import StockRoute from "./routes/stock-route.js";
 import CartRoute from "./routes/cart-route.js";
 import WishlistRoute from "./routes/wishlist-route.js";
+import OrderRoute from "./routes/order-route.js";
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use("/size", SizeRoute);
 app.use("/stock", StockRoute);
 app.use("/cart", CartRoute);
 app.use("/wishlist", WishlistRoute);
+app.use("/order", OrderRoute);
 
 app.listen(process.env.API_PORT, () =>
   console.log("Server berhasil dijalankan.")
