f8c0d2a541b5aba1252e6e51ccbe57bace382c6a
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-11 19:47:25 +0700

    feat(server): Add functionality to retrieve all orders from users

--- server/api/controllers/order-controller.js
+++ server/api/controllers/order-controller.js
@@ -18,26 +18,39 @@ export const getOrderByIdUser = async (req, res) => {
   }
 };
 
+export const getAllOrder = async (req, res) => {
+  try {
+    const result = await pool.query(
+      `
+      SELECT u.username, p.name_product, s.name_size, o.quantity, o.payment_method, o.address, o.status
+      FROM orders o
+      JOIN users u ON o.id_user = u.id
+      JOIN products p ON o.id_product = p.id
+      JOIN sizes s ON o.id_size = s.id 
+    `
+    );
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
 export const addOrderUser = async (req, res) => {
   const { id_user, orders, payment_method, address, status } = req.body;
   try {
     for (const order of orders) {
       const { id_product, id_size, quantity } = order;
       await pool.query(
-        "INSERT INTO orders (id_user, id_product, id_size, quantity, payment_method, address, status) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-        [
-          id_user,
-          id_product,
-          id_size,
-          quantity,
-          payment_method,
-          address,
-          status,
-        ]
+        "INSERT INTO orders (id_user, id_product, id_size, quantity, payment_method, address) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+        [id_user, id_product, id_size, quantity, payment_method, address]
       );
     }
     res.status(200).json({ msg: "Pesanan telah berhasil" });
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
+};
+
+const updateStatus = async (req, res) => {
+  // const {}
 };
--- server/api/routes/order-route.js
+++ server/api/routes/order-route.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth-middleware.js";
-import { addOrderUser, getOrderByIdUser } from "../controllers/order-controller.js";
+import {
+  addOrderUser,
+  getAllOrder,
+  getOrderByIdUser,
+} from "../controllers/order-controller.js";
 
 const router = express.Router();
 
-router.get("/get/:id", verifyToken, getOrderByIdUser); 
-router.post("/add", verifyToken, addOrderUser); 
+router.get("/get/:id", verifyToken, getOrderByIdUser);
+router.get("/get-all", verifyToken, getAllOrder);
+router.post("/add", verifyToken, addOrderUser);
 // router.put("/update/:id", verifyToken, updateCart); // Router untuk mengubah data keranjang berdasarkan id
 // router.delete("/delete/:id", verifyToken, deleteCart); // Router untuk menghapus data keranjang berdasarkan id
 
