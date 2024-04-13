76bcc2b0dbc87df785b0fa3aaa7cd35af740ed31
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-13 13:26:42 +0700

    fix(client): Update error page for admin section

--- client/src/pages/ErrorPageAdmin.jsx
+++ client/src/pages/ErrorPageAdmin.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function ErrorPageAdmin() {
+export default function ErrorPage() {
   return (
     <div className="h-screen flex flex-col justify-center items-center font-KumbhSans">
       <h1 className="text-4xl font-bold text-black mb-4">404 Not Found</h1>
@@ -8,7 +8,7 @@ export default function ErrorPageAdmin() {
         Maaf, halaman yang Anda cari tidak ditemukan.
       </p>
       <Link
-        to="/"
+        to={localStorage.getItem("role") === "admin" ? "/admin" : "/"}
         className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
       >
         Kembali ke Beranda
