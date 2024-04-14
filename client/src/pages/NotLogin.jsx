0a5e68bcd452eadaed81930145b903f7e1be7f19
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-14 12:50:17 +0700

    fix(client): Fix layout and logic on notlogin page

--- client/src/pages/NotLogin.jsx
+++ client/src/pages/NotLogin.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function NotLogin() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
       <h1 className="text-4xl font-bold text-gray-800 mb-8">Oops!</h1>
       <p className="text-lg text-gray-600 mb-4">
         Anda belum login. Silakan login terlebih dahulu untuk melanjutkan.
       </p>
-      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      <Link
+        to="/login"
+        className="bg-black border border-black hover:bg-white hover:text-black text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+      >
         Login Sekarang
-      </button>
+      </Link>
     </div>
   );
 }
