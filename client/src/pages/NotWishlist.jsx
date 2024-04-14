b5e8a51ee79457012923026a70813a4eeb86cdab
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-14 13:39:14 +0700

    feat(client): Update layout for Not Wishlist page

--- client/src/pages/NotWishlist.jsx
+++ client/src/pages/NotWishlist.jsx
@@ -1,7 +1,26 @@
+import { GoBookmark } from "react-icons/go";
+import { Link } from "react-router-dom";
 export default function NotWishlist() {
   return (
-    <div>
-      <h1>Tidak ada wishlist</h1>
+    <div className="flex flex-col items-center py-4 gap-5">
+      <div className="relative">
+        <div className="absolute right-2 -top-2 text-white px-3 py-1 bg-black rounded-full text-5xl">
+          0
+        </div>
+        <GoBookmark className="text-9xl" />
+      </div>
+      <h1 className="text-2xl font-semibold">
+        Your Wishlist Is Currently Empty
+      </h1>
+      <div className="flex items-center gap-1">
+        Click the <GoBookmark /> icons to add products
+      </div>
+      <Link
+        to="/shop"
+        className="bg-white text-black hover:bg-black hover:text-white py-3 px-7 outline outline-[1px]"
+      >
+        RETURN TO SHOP
+      </Link>
     </div>
   );
 }
--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { AllContext } from "../App";
 import CardWishlist from "../components/CardWishlist";
 import NotLogin from "./NotLogin";
+import { GoBookmark } from "react-icons/go";
+import NotWishlist from "./NotWishlist";
 
 export default function Wishlist() {
   const { wishlist } = useContext(AllContext);
@@ -25,37 +27,44 @@ export default function Wishlist() {
   ];
   if (localStorage.getItem("token")) {
     return (
-      <div className="flex flex-col gap-5 py-5 bg-gray-100">
+      <div className="flex flex-col gap-5 py-5 ">
         <h1 className="text-center font-bold tracking-widest text-2xl">
           WISHLIST
         </h1>
-        <div className="flex justify-between pt-4 px-5">
-          <div className="flex w-full flex-row items-center">
-            {headingWishlist.map((h) => (
-              <h1
-                key={h.id}
-                className="w-1/4 text-center text-base font-extrabold tracking-wider"
-              >
-                {h.name}
-              </h1>
-            ))}
+        {wishlist.length > 0 ? (
+          <div>
+            {" "}
+            <div className="flex justify-between pt-4 px-5">
+              <div className="flex w-full flex-row items-center">
+                {headingWishlist.map((h) => (
+                  <h1
+                    key={h.id}
+                    className="w-1/4 text-center text-base font-extrabold tracking-wider"
+                  >
+                    {h.name}
+                  </h1>
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div className="px-10">
+                {wishlist.map((w) => (
+                  <CardWishlist
+                    key={w.id}
+                    id={w.id}
+                    id_product={w.id_product}
+                    image_1={w.image_1}
+                    name_product={w.name_product}
+                    price={w.price}
+                    status={w.status}
+                  />
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col">
-          <div className="px-10">
-            {wishlist.map((w) => (
-              <CardWishlist
-                key={w.id}
-                id={w.id}
-                id_product={w.id_product}
-                image_1={w.image_1}
-                name_product={w.name_product}
-                price={w.price}
-                status={w.status}
-              />
-            ))}
-          </div>
-        </div>
+        ) : (
+          <NotWishlist />
+        )}
       </div>
     );
   } else {
