1ac5a891826dbe298c76ecf6872d6b0b80593e00
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-16 18:34:45 +0700

    fix(server): Fix route for stock section

--- server/api/routes/stock-route.js
+++ server/api/routes/stock-route.js
@@ -11,7 +11,7 @@ import {
 const router = express.Router();
 
 router.get("/get-all", verifyToken, getAllStock); // Router untuk mendapatkan semua stok
-router.get("/get-size/:id", verifyToken, getSizeBydIdProduct);
+router.get("/get-size/:id", getSizeBydIdProduct);
 router.post("/add", verifyToken, addStock); // Router untuk menambahkan data stok
 router.put("/update/:id", verifyToken, updateStock); // Router untuk mengubah data stok berdasarkan id
 router.delete("/delete/:id", verifyToken, deleteStock); // Router untuk menghapus data stok berdasarkan id
