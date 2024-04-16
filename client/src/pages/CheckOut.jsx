089da4c234d06ab658c1d7b845651590cdb9e14b
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-16 16:24:26 +0700

    feat(client): Create checkout page

---
+++ client/src/pages/CheckOut.jsx
@@ -0,0 +1,10 @@
+export default function CheckOut() {
+  return (
+    <div className="flex flex-col py-5">
+      <h1 className="text-center font-bold tracking-widest text-2xl">
+        CHECKOUT
+      </h1>
+      <div></div>
+    </div>
+  );
+}
