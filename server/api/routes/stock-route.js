354a9afee651ebf51157c9c2a350341122ef993b
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-16 13:56:05 +0700

    feat(server): Add route to fetch size names in stock route

--- server/api/routes/stock-route.js
+++ server/api/routes/stock-route.js
@@ -4,12 +4,14 @@ import {
   addStock,
   deleteStock,
   getAllStock,
+  getSizeBydIdProduct,
   updateStock,
 } from "../controllers/stock-controller.js";
 
 const router = express.Router();
 
 router.get("/get-all", verifyToken, getAllStock); // Router untuk mendapatkan semua stok
+router.get("/get-size/:id", verifyToken, getSizeBydIdProduct);
 router.post("/add", verifyToken, addStock); // Router untuk menambahkan data stok
 router.put("/update/:id", verifyToken, updateStock); // Router untuk mengubah data stok berdasarkan id
 router.delete("/delete/:id", verifyToken, deleteStock); // Router untuk menghapus data stok berdasarkan id
