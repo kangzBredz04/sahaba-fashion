29a7b7c76c2a5cd7837c622f8eecfd6a1927ef1c
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-17 20:24:37 +0700

    feat (server): Add deployment link to Sahaba Fashion in index.js

--- server/api/index.js
+++ server/api/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://sahaba-fashion.vercel.app"],
     credentials: true,
   })
 );
