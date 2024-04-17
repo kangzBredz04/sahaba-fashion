23c84e3f4ddac4a491ed765358ff340b62d2dbe1
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-17 11:07:21 +0700

    refactor(server): Update register logic in auth controller

--- server/api/controllers/auth-controller.js
+++ server/api/controllers/auth-controller.js
@@ -4,17 +4,16 @@ import { pool } from "../config/db.js";
 
 // Controller untuk daftar akun (register)
 export const registerAccount = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { first_name, last_name, username, email, password } = req.body;
   try {
     const hashPassword = await argon2.hash(password);
     const result = await pool.query(
-      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
-      [username, email, hashPassword]
+      "INSERT INTO users (first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [first_name, last_name, username, email, hashPassword]
     );
     res.status(201).json({ msg: "Pendaftaran berhasil", data: result.rows[0] });
   } catch (error) {
-    // console.log(error);
-    res.status(500).json({ error });
+    res.status(500).json({ msg: error.message });
   }
 };
 
@@ -48,11 +47,10 @@ export const loginAccount = async (req, res) => {
     );
 
     if (result.rows[0]) {
-      // console.log(result.rows[0].password);
-      // console.log(password);
-      // console.log(await argon2.hash(password));
-      // Verifikasi antara password yang diinputkan di client dengan database
-      const isPasswordValid = argon2.verify(result.rows[0].password, password);
+      const isPasswordValid = await argon2.verify(
+        result.rows[0].password,
+        password
+      );
 
       if (isPasswordValid) {
         const token = jwt.sign(result.rows[0], process.env.SECRET_KEY);
