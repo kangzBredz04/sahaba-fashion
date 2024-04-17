bb65f270294506d64c30df12a26e02d71812bd8e
Wahyu Jebredz <wabredz1234@gmail.com>
2024-04-17 11:05:08 +0700

    refactor(client): Update logic and UI for Register Page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,16 @@
+/* eslint-disable no-unused-vars */
 import { useContext } from "react";
 import { api } from "../utils";
 import { AllContext } from "../App";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 export default function Register() {
   const { register, setRegister } = useContext(AllContext);
+  const [user, setUser] = useOutletContext();
+
+  const navigate = useNavigate();
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto font-KumbhSans">
       <div className="flex justify-center items-center h-screen">
         <div className="w-full md:w-1/3 lg:w-1/2 p-6">
           <h1 className="text-xl font-bold mb-6 font-KumbhSans text-center">
@@ -14,20 +19,77 @@ export default function Register() {
           <form
             onSubmit={(e) => {
               e.preventDefault();
-              api
-                .post("/auth/register", register)
-                .then(async (res) => {
-                  alert(res.msg);
-                  window.location.href = "/";
-                })
-                .catch((e) => {
-                  console.log(e);
-                });
+              api.post("/auth/register", register).then((res) => {
+                alert(res.msg);
+                window.location.href = "/";
+              });
+              const data = api.get("/auth/my-account").then((res) => {
+                // console.log(res.data.role);
+                setUser(res.data);
+                localStorage.setItem("role", res.data.role);
+                localStorage.setItem("id", res.data.id);
+                if (res.data.role === "admin") {
+                  console.log("Masuk sebagai admin");
+                  navigate("/admin");
+                  // window.location.reload();
+                } else {
+                  console.log("Masuk sebagai user");
+                  navigate("/");
+                  // window.location.reload();
+                }
+              });
+              setUser(data);
             }}
           >
+            <div className=" flex flex-row gap-4">
+              <div className="mb-4 grow">
+                <label
+                  className="block text-xs font-semibold mb-2"
+                  htmlFor="first_name"
+                >
+                  FIRST NAME *
+                </label>
+                <input
+                  className="w-full px-3 py-2 border border-gray-300 focus:border-black focus:outline-none focus:ring-black focus:font-KumbhSans"
+                  type="text"
+                  id="first_name"
+                  name="first_name"
+                  value={register.first_name}
+                  onChange={(e) =>
+                    setRegister({
+                      ...register,
+                      first_name: e.target.value,
+                    })
+                  }
+                  required
+                />
+              </div>
+              <div className="mb-4 grow">
+                <label
+                  className="block text-xs font-semibold mb-2"
+                  htmlFor="last_name"
+                >
+                  LAST NAME *
+                </label>
+                <input
+                  className="w-full px-3 py-2 border border-gray-300 focus:border-black focus:outline-none focus:ring-black focus:font-KumbhSans"
+                  type="text"
+                  id="last_name"
+                  name="last_name"
+                  value={register.last_name}
+                  onChange={(e) =>
+                    setRegister({
+                      ...register,
+                      last_name: e.target.value,
+                    })
+                  }
+                  required
+                />
+              </div>
+            </div>
             <div className="mb-4">
               <label
-                className="block text-xs font-semibold mb-2 font-KumbhSans"
+                className="block text-xs font-semibold mb-2"
                 htmlFor="username"
               >
                 USERNAME *
@@ -44,11 +106,12 @@ export default function Register() {
                     username: e.target.value,
                   })
                 }
+                required
               />
             </div>
             <div className="mb-4">
               <label
-                className="block text-xs font-semibold mb-2 font-KumbhSans"
+                className="block text-xs font-semibold mb-2 "
                 htmlFor="email"
               >
                 EMAIL ADDRESS *
@@ -65,11 +128,12 @@ export default function Register() {
                     email: e.target.value,
                   })
                 }
+                required
               />
             </div>
             <div className="mb-4">
               <label
-                className="block text-xs font-semibold mb-2 font-KumbhSans"
+                className="block text-xs font-semibold mb-2"
                 htmlFor="password"
               >
                 PASSWORD *
@@ -86,28 +150,29 @@ export default function Register() {
                     password: e.target.value,
                   })
                 }
+                required
               />
             </div>
             <div className="mb-4">
-              <p className="font-KumbhSans text-xs">
+              <p className="text-xs">
                 Data pribadi Anda akan digunakan untuk mendukung aktivitas Anda
                 di web ini, untuk mengelola akses ke akun, dan untuk tujuan lain
                 yang dijelaskan dalam{" "}
                 <a href="" className="underline">
                   privacy policy
-                </a>
+                </a>{" "}
                 kami.
               </p>
             </div>
             <button
               type="submit"
-              className="w-full px-4 py-4 font-bold mb-4 text-white bg-black"
+              className="w-full px-4 py-4 font-bold mb-4 text-white bg-black hover:bg-white hover:text-black outline"
             >
               Register
             </button>
             <div className="mb-4 text-center">
-              <p className="font-KumbhSans text-xs">
-                You A Member?
+              <p className="text-xs">
+                You A Member ?{" "}
                 <a href="/login" className="underline">
                   Login
                 </a>
